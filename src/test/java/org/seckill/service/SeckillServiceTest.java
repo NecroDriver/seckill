e3b9beb8fb1645fc559148966a3419d87926487d
mafh <1193482868@qq.com>
2017-09-29 18:19:23 +0800

    commit

---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,38 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+    }
+
+    @Test
+    public void getById() throws Exception {
+    }
+
+    @Test
+    public void exportSeckillUrl() throws Exception {
+    }
+
+    @Test
+    public void executeSeckill() throws Exception {
+    }
+
+}
