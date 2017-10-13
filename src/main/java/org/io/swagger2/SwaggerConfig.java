ae162c09fcaa71f45e6141d74299ca719bc54fa4
mafh <1193482868@qq.com>
2017-10-13 18:05:55 +0800

    swagger

---
+++ src/main/java/org/io/swagger2/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package org.io.swagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@WebAppConfiguration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.seckill.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("使用Swagger2构建RESTful APIs")
+                .description("更多内容描述敬请期待http://www.mafh.xin:8088/")
+                .termsOfServiceUrl("http://www.mafh.xin:8088/")
+                .contact(new Contact("mafh","http://www.mafh.xin:8088/","necrodriver@foxmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
